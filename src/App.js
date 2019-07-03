967e3c853d199cea1689f80e10dd86296090f23d
bencyn <bensonnjung39@gmail.com>
2019-07-03 11:44:57 +0300

    task(chore):integrate-sass-processing         - add sass loader         - add sass package   [Finishes #166980300]

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import routes from "./routes";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import "./assets/css/app.css";
+import "./assets/css/app.scss";
 
 function App() {
   return (
